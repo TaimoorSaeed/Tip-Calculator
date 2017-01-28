d65813e3551348f134ec3ffd2ceeaa5db02b9da4
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-01-28 21:11:07 +0500

    functionality

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -12,20 +12,39 @@
 
     @IBOutlet weak var txtFeildValue: UITextField!
 
+        @IBOutlet weak var slider: UISlider!
+        
+        @IBOutlet weak var sliderlabel: UILabel!
+        
+        
+        @IBOutlet weak var TipLabel: UILabel!
+        
+        @IBOutlet weak var txtFeildLabel: UITextField!
+        
+       // MARK: NOTICE
+        
     override func viewDidLoad() {
     super.viewDidLoad()
 
+        TipLabel.text = "0"
     let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 
     view.addGestureRecognizer(tap)
-
-
-
     }
+        
+        
+        @IBAction func sliderValueChanged(_ sender: UISlider) {
+            var currentValue = Int(sender.value)
+            
+            TipLabel.text = String(currentValue)
+            
+            sliderlabel.text = "Tip(" + String(currentValue) + "%)"
+        }
 
     func dismissKeyboard() {
     view.endEditing(true)
     }
+        
 
     }
 
